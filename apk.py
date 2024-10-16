33f82c11605adf9b7218e493c00866b2d84fe694
shintaputrii <shintacantikk2@gmail.com>
2024-10-16 20:55:10 +0700

    Update apk.py

--- apk.py
+++ apk.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from numpy import array
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxdScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_absolute_percentage_error
